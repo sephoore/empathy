7a48b711c113d83ed744f5474b4c81cdd0e65ebd
tharnuk <private_dev@gmx.de>
2017-01-23 22:13:14 +0100

    alphaCore seph: first event handler test, guild hello, slash command

--- empathy.lua
+++ empathy.lua
@@ -12,9 +12,34 @@
 -- ************************************************************
 
 
-local frame = CreateFrame("FRAME", "FooAddonFrame");
-frame:RegisterEvent("MINIMAP_PING");
-local function eventHandler(self, event, ...)
- print("Peng");
-end
-frame:SetScript("OnEvent", eventHandler);
+-- ################# global vars ########################
+
+	SLASH_EMPATHY1, SLASH_EMPATHY2 = '/empgui', '/empathy';  -- open GUI
+
+
+
+-- #################  functions  ########################
+
+	-- ++++++++ SETTING GUI +++++++++
+
+		function SlashCmdList.EMPATHY(msg, editbox) -- 4.
+		 --CODE HERE ;
+		end	
+
+
+	-- +++++++++++ GUILD ++++++++++++
+		-- hello
+		
+		local frame = CreateFrame("FRAME", "FooAddonFrame");
+		frame:RegisterEvent("PLAYER_LOGIN");
+		local function eventHandler(self, event, ...)
+		 SendChatMessage(default_hello, "GUILD", nil, nil);
+		end
+		frame:SetScript("OnEvent", eventHandler);
+
+	-- +++++++++++ PARTY ++++++++++++
+	-- +++++++++++ RAID +++++++++++++
+	-- ++++++++++++ PVP +++++++++++++
+
+
+
