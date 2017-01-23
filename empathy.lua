81bf5e4524f933c3718a8abc4821826202751204
tharnuk <private_dev@gmx.de>
2017-01-23 22:49:39 +0100

    alphaCore seph: first structure

--- empathy.lua
+++ empathy.lua
@@ -12,9 +12,25 @@
 -- ************************************************************
 
 
+-- ############### register events ######################
+
+	frame:RegisterEvent("ADDON_LOADED"); -- check if addon is loaded completely
+	frame:RegisterEvent("PLAYER_LOGIN"); -- register my login
+
+-- ############# init saved variables ###################	
+	
+	function frame:OnEvent(event, arg1)
+	 if event == "ADDON_LOADED" and arg1 == "Empathy" then	
+		
+		-- check if either player specific or global saved variables are existing
+		if (empathy_svc or empathy_sv) == nil then
+		
+		end
+	end
 -- ################# global vars ########################
 
 	SLASH_EMPATHY1, SLASH_EMPATHY2 = '/empgui', '/empathy';  -- open GUI
+
 
 
 
@@ -30,10 +46,12 @@
 	-- +++++++++++ GUILD ++++++++++++
 		-- hello
 		
+		
+		
 		local frame = CreateFrame("FRAME", "FooAddonFrame");
-		frame:RegisterEvent("PLAYER_LOGIN");
+		
 		local function eventHandler(self, event, ...)
-		 SendChatMessage(default_hello, "GUILD", nil, nil);
+		 SendChatMessage(guild_hello, "GUILD", nil, nil);
 		end
 		frame:SetScript("OnEvent", eventHandler);
 
